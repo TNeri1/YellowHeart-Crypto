cd1df6ce38368131e0bf8721a181d7320aab062f
Use your GitHub username <Enter your email address>
2020-11-25 11:37:19 -0700

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import BuyForm from './BuyForm'
+
+class Main extends Component { 
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      output: '0'
+    }
+  }
+
+  render() {
+    return (
+      <div id="content">
+        
+          <div className="card mb-4" >
+
+          <div className="card-body">
+
+          <BuyForm 
+            ethBalance={this.props.ethBalance}
+            tokenBalance={this.props.tokenBalance}
+            buyTokens={this.props.buyTokens}
+          />
+
+          </div>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Main;
